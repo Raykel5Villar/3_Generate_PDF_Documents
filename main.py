bb150908552ce57bf26974c1fe1b2d44d27712fb
Raykel5Villar <raykelvillar@gmail.com>
2024-05-05 19:49:43 -0400

    pdf generator

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align="L", ln=1)
-    pdf.line(x1=, y1=, x2=, y2=)
+    pdf.line(x1=10, y1=21, x2=200, y2=21)
+
+    for i in range(row['Pages'] - 1):
+        pdf.add_page()
+        pdf.line(x1=10, y1=21, x2=200, y2=21)
 
 pdf.output("output.pdf")
